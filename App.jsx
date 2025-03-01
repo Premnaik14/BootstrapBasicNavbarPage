a70a9a488c0c86e681b38a06afb76300912cbe35
Prem Naiknavare <90269160+Premnaik14@users.noreply.github.com>
2025-03-01 14:20:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProductCard from './Components/ProductCard.jsx'
+import MyComponent from './components/MyComponent.jsx'
+import BasicPage from './components/BasicPage.jsx'
+
+
+function App(){
+    return(
+        <div>
+            {/* <ProductCard/>
+            <MyComponent/> */}
+            <BasicPage/>
+        </div>
+    )
+} 
+
+export default App
